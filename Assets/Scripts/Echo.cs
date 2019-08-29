b768cb473c9d7fa6a7d4290470435441d243b203
0YSQD05YY1BMCM6\Administrator <huiyao8761380@gmail.com>
2019-08-29 16:07:29 +0800

    1

--- Assets/Scripts/Echo.cs
+++ Assets/Scripts/Echo.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Sockets;
 using UnityEngine.UI;
+using System;
+
 public class Echo : MonoBehaviour {
 
 	//定义套接字
@@ -10,17 +12,40 @@ public class Echo : MonoBehaviour {
 	//UGUI
 	public InputField InputFeld;
 	public Text text;
+	//接收缓冲区
+	byte[] readBuff = new byte[1024]; 
+	string recvStr = "";
 
-	//点击连接按钮
-	public void Connetion()
-	{
-		//Socket
-		socket = new Socket(AddressFamily.InterNetwork,
-			SocketType.Stream, ProtocolType.Tcp);
-		//Connect
-		socket.Connect("127.0.0.1", 8888);
+	//Connect回调
+	public void ConnectCallback(IAsyncResult ar){
+		try{
+			Socket socket = (Socket) ar.AsyncState;
+			socket.EndConnect(ar);
+			Debug.Log("Socket Connect Succ ");
+			socket.BeginReceive( readBuff, 0, 1024, 0,
+				ReceiveCallback, socket);
+		}
+		catch (SocketException ex){
+			Debug.Log("Socket Connect fail" + ex.ToString());
+		}
 	}
+	
+		//Receive回调
+	public void ReceiveCallback(IAsyncResult ar){
+		try {
+			Socket socket = (Socket) ar.AsyncState;
+			int count = socket.EndReceive(ar);
+			string s = System.Text.Encoding.UTF8.GetString(readBuff, 0, count);
+			recvStr = s + "\n" + recvStr;
 
+			socket.BeginReceive( readBuff, 0, 1024, 0,
+				ReceiveCallback, socket);
+		}
+		catch (SocketException ex){
+			Debug.Log("Socket Receive fail" + ex.ToString());
+		}
+	}
+	
 	//点击发送按钮
 	public void Send()
 	{
